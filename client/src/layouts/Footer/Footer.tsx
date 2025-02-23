16d7f1bdecd387bbdf39e136d93c9b9999a6d747
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-23 14:50:47 +0200

    added responsive footer

--- client/src/layouts/Footer/Footer.tsx
+++ client/src/layouts/Footer/Footer.tsx
@@ -1,8 +1,162 @@
+import styles from "./Footer.module.css";
+
+import logo from "../../assets/icons/logo_mix.png";
+import phone from "../../assets/icons/phone.png";
+import mail from "../../assets/icons/mail.png";
+import location from "../../assets/icons/location.png";
+import twitter from "../../assets/icons/twitter.png";
+import facebook from "../../assets/icons/facebook.png";
+import pinterest from "../../assets/icons/pinterest.png";
+import instagram from "../../assets/icons/instagram.png";
+import { Link } from "react-router-dom";
+
+const Icon = ({ image }: { image: string }) => {
+  return (
+    <img
+      src={image}
+      width={16}
+      height={16}
+      alt={image}
+      className={styles.icon}
+      loading="lazy"
+    />
+  );
+};
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.wrapper}>
+      {/* upper section */}
+      <div className={styles.upper}>
+        {/* left side */}
+        <div className={styles.left}>
+          {/* upper left side */}
+          <div className={styles.logoContainer}>
+            <img src={logo} alt="logo" loading="lazy" className={styles.logo} />
+            <p className={styles.logoText}>
+              Welcome to our consultancy agency.
+            </p>
+          </div>
 
-export default Footer
+          {/* lower left side */}
+          <ul className={styles.contactInfo}>
+            <li>
+              <Icon image={phone} />
+              <span>+966 583433143 - +971 507505539 </span>
+            </li>
+
+            <li>
+              <Icon image={mail} />
+              <span>info@Futureinsights.ae</span>
+            </li>
+
+            <li>
+              <Icon image={location} />
+              <span>13-47 Royal House, Hor Al Anz East, Dubai, UAE</span>
+            </li>
+          </ul>
+        </div>
+
+        {/* middle  */}
+        <div className={styles.middle}>
+          <p className={styles.sectionTitle}>Explore</p>
+          <div className={styles.columns}>
+            <ul className={styles.first}>
+              <li>
+                <Link to="/" className={styles.link}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/course-catalogue" className={styles.link}>
+                  Course Catalogue
+                </Link>
+              </li>
+              <li>
+                <Link to="/teacher-signup" className={styles.link}>
+                  Become a Teacher
+                </Link>
+              </li>
+              <li>
+                <Link to="/my-courses" className={styles.link}>
+                  My Courses
+                </Link>
+              </li>
+              <li>
+                <Link to="/consultation" className={styles.link}>
+                  Consultation
+                </Link>
+              </li>
+            </ul>
+
+            <ul className={styles.second}>
+              <li>
+                <Link to="/support" className={styles.link}>
+                  Support
+                </Link>
+              </li>
+              <li>
+                <Link to="/terms" className={styles.link}>
+                  Terms of use
+                </Link>
+              </li>
+              <li>
+                <Link to="/privacy" className={styles.link}>
+                  Privacy policy
+                </Link>
+              </li>
+              <li>
+                <Link to="/help" className={styles.link}>
+                  Help
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className={styles.right}>
+          <p className={styles.sectionTitle}>Newsletter</p>
+
+          <div className={styles.newsletterContainer}>
+            <p className={styles.newsLetterText}>
+              Subscribe for latest articles and resources
+            </p>
+
+            <div className={styles.emailContainer}>
+              <input
+                type="email"
+                placeholder="Email address"
+                className={styles.email}
+                required
+              />
+
+              <button className={styles.subscribe}>Register</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* lower section  */}
+      <div className={styles.lower}>
+        <p>&copy; 2025 Future Insights</p>
+        <ul>
+          <li>
+            <Icon image={twitter} />
+          </li>
+          <li>
+            <Icon image={facebook} />
+          </li>
+          <li>
+            <Icon image={pinterest} />
+          </li>
+          <li>
+            <Icon image={instagram} />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
