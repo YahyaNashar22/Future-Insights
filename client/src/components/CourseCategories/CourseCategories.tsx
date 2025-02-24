afd8433a9c58040e31f5d72f6cca31d71954d3dd
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-24 22:42:51 +0200

    category links

--- client/src/components/CourseCategories/CourseCategories.tsx
+++ client/src/components/CourseCategories/CourseCategories.tsx
@@ -1,8 +1,13 @@
+import { FC } from "react";
+
 import styles from "./CourseCategories.module.css";
 
 import category from "../../assets/icons/category.png";
+import ICategory from "../../interfaces/ICategory";
 
-const CourseCategories = () => {
+const CourseCategories: FC<{ categories: ICategory[] | null }> = ({
+  categories,
+}) => {
   return (
     <section className={styles.wrapper}>
       <img
@@ -12,7 +17,9 @@ const CourseCategories = () => {
         className={styles.category}
       />
       <ul className={styles.categoryContainer}>
-        
+        {categories?.map((category) => {
+          return <li key={category.id} className={styles.categoryTitle}>{category.title}</li>;
+        })}
       </ul>
     </section>
   );
--- client/src/pages/CourseCatalogue/CourseCatalogue.tsx
+++ client/src/pages/CourseCatalogue/CourseCatalogue.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import styles from "./CourseCatalogue.module.css";
 
 import CourseCatalogueHero from "../../components/CourseCatalogueHero/CourseCatalogueHero";
+import ICategory from "../../interfaces/ICategory";
+import CourseCategories from "../../components/CourseCategories/CourseCategories";
+import Loading from "../../components/Loading/Loading";
 
 const CourseCatalogue = () => {
+  const backend = import.meta.env.VITE_BACKEND;
+
+  const [categories, setCategories] = useState<ICategory[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(`${backend}/category/get-all`);
+
+        setCategories(response.data.payload);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, [backend]);
+
   return (
     <main className={styles.wrapper}>
       <CourseCatalogueHero />
+
+      {loading ? <Loading /> : <CourseCategories categories={categories} />}
     </main>
   );
 };
