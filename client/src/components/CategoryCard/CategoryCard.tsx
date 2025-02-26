6a2273a5e2bd15a155d6c6cf3b27dbc40160aabe
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-26 23:29:34 +0200

    fixed fallback values for category card

--- client/src/components/CategoryCard/CategoryCard.tsx
+++ client/src/components/CategoryCard/CategoryCard.tsx
@@ -6,9 +6,7 @@ import categoryTitleLeading from "../../assets/icons/category_title_leading.png"
 import { Link } from "react-router-dom";
 import ICategoryCard from "../../interfaces/ICategoryCard";
 
-const CategoryCard: FC<ICategoryCard> = ({
-  selectedCategory, showButton
-}) => {
+const CategoryCard: FC<ICategoryCard> = ({ selectedCategory, showButton }) => {
   const backend = import.meta.env.VITE_BACKEND;
   return (
     <div
@@ -24,10 +22,10 @@ const CategoryCard: FC<ICategoryCard> = ({
         <span className={styles.leading}>
           <img src={categoryTitleLeading} alt="leading" loading="lazy" />
         </span>
-        {selectedCategory?.title ? selectedCategory.title : "Select a Category"}
+        {selectedCategory ? selectedCategory.title : "Select a Category"}
       </h2>
       <p className={styles.description}>
-        {selectedCategory?.description
+        {selectedCategory
           ? selectedCategory?.description
           : "Please select a category to read more about it and discover the courses within it"}
       </p>
