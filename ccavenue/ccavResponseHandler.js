8af584fc7914960e30a166badc13accf4c6fd11a
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-27 23:22:18 +0300

    course id log

--- ccavenue/ccavResponseHandler.js
+++ ccavenue/ccavResponseHandler.js
@@ -3,6 +3,10 @@ import { decrypt } from './Crypto.js';
 export const ccavResponseHandler = (req, res) => {
     console.log("Received CCAvenue response:", req.body);
     const encryptedResponse = req.body.encResp;
+    const courseId = req.body.courseId;
+
+    console.log('courseId: ', courseId)
+
 
     try {
         console.log('Encrypted Response:', encryptedResponse);
@@ -11,16 +15,22 @@ export const ccavResponseHandler = (req, res) => {
         console.log("Decrypted Response:", decrypted);
 
         const params = Object.fromEntries(decrypted.split('&').map(pair => pair.split('=')));
-        console.log("Decrypted params:", params); 
+        console.log("Decrypted params:", params);
 
         const orderStatus = params.order_status;
         console.log("Order Status:", orderStatus);
-        
-        // Optional: Save to DB
 
-        // Redirect to your React frontend with result
-        const redirectUrl = `http://localhost:5173/payment-status?status=${orderStatus}`;
-        res.redirect(redirectUrl);
+        if (orderStatus === "Failure") {
+            const redirectUrl = `${process.env.CLIENT_URL}`;
+            // Redirect to your React frontend with result
+            res.redirect(redirectUrl);
+        } else {
+            console.log('courseId: ', courseId)
+        }
+
+
+
+
     } catch (err) {
         console.error("Decryption error:", err);
         res.status(500).send("Something went wrong.");
