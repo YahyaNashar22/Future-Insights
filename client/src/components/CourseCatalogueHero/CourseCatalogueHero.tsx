d95f556523b3fcf55adfa2b6b8c1b22d6a29537f
YahyaNashar22 <yahyanashar22@gmail.com>
2025-04-29 10:29:29 +0300

    translated course catalogue hero and loading

--- client/src/components/CourseCatalogueHero/CourseCatalogueHero.tsx
+++ client/src/components/CourseCatalogueHero/CourseCatalogueHero.tsx
@@ -4,13 +4,15 @@ import BlueBeige from "../BlueBeige/BlueBeige";
 import CourseCatalogueHeroImage from "../CourseCatalogueHeroImage/CourseCatalogueHeroImage";
 
 import bubble from "../../assets/icons/bubble.png";
+import { useTranslation } from "react-i18next";
 
 const CourseCatalogueHero = () => {
+  const { t } = useTranslation();
   return (
     <section className={styles.wrapper}>
       <img src={bubble} alt="bubble" className={styles.bubble} loading="lazy" />
       <h1 className={styles.title}>
-        Course <br /> Catalogue
+        {t('course-catalogue-hero-1')} <br /> {t('course-catalogue-hero-2')}
       </h1>
       <div className={styles.container}>
         <CourseCatalogueHeroImage />
--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,10 +1,12 @@
+import { useTranslation } from "react-i18next";
 import styles from "./Loading.module.css"; // Import CSS Module
 
 const Loading = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.loadingContainer}>
       <div className={styles.spinner}></div>
-      <p className={styles.loadingText}>Loading...</p>
+      <p className={styles.loadingText}>{t("loading")}</p>
     </div>
   );
 };
