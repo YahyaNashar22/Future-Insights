3f35ed9f34db689a267badba1beabd2da314551d
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-01 15:06:22 +0300

    support translated

--- client/src/pages/Support/Support.tsx
+++ client/src/pages/Support/Support.tsx
@@ -1,41 +1,43 @@
+import { useTranslation } from "react-i18next";
 import styles from "./Support.module.css";
 
 const Support = () => {
+  const { t } = useTranslation();
   return (
     <main className={styles.wrapper}>
       <section className={styles.supportSection}>
-        <h1 className={styles.title}>Support</h1>
+        <h1 className={styles.title}>{t("support-title")}</h1>
 
         <div className={styles.section}>
-          <h2 className={styles.subtitle}>1. Contacting Support</h2>
+          <h2 className={styles.subtitle}>{t("support-1-title")}</h2>
           <p className={styles.text}>
-            • For technical issues, account inquiries, or billing support,
-            contact us at
+            {t("support-1-desc-1")}
             <a href="mailto:info@futureinsights.ae" className={styles.link}>
               {" "}
-              info@futureinsights.ae
+              {t("support-1-desc-2")}
             </a>
             .
           </p>
           <p className={styles.text}>
-            • Support is available{" "}
-            <strong className={styles.strong}>24/7</strong>.
+            {t("support-1-desc-3")}{" "}
+            <strong className={styles.strong}>{t("support-1-desc-4")}</strong>.
           </p>
         </div>
 
         <div className={styles.section}>
-          <h2 className={styles.subtitle}>2. FAQs & Help Center</h2>
+          <h2 className={styles.subtitle}>{t("support-2-title")}</h2>
           <p className={styles.text}>
-            • Users can find answers to common questions in our Help Center at
-            <span className={styles.link}> Help Center</span>.
+            {t("support-2-desc-1")}
+            <span className={styles.link}> {t("support-2-desc-2")}</span>.
           </p>
         </div>
 
         <div className={styles.section}>
-          <h2 className={styles.subtitle}>3. Response Time</h2>
+          <h2 className={styles.subtitle}>{t("support-3-title")}</h2>
           <p className={styles.text}>
-            • Our team strives to respond to inquiries within{" "}
-            <strong className={styles.strong}>5</strong> business days.
+            {t("support-3-desc-1")}{" "}
+            <strong className={styles.strong}>{t("support-3-desc-2")}</strong>{" "}
+            {t("support-3-desc-3")}
           </p>
         </div>
       </section>
